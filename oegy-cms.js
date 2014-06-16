05c823c255389698350c55d5652ea9881502d0ef
Rasmus Back <rasmus.back@reaktor.fi>
2014-06-16 17:44:29 +0300

    WIP: support for sites

--- oegy-cms.js
+++ oegy-cms.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var enchilada = require('enchilada');
 var lessMiddleware = require('less-middleware');
+var _ = require('lodash')
+var fs = require('fs')
+var oegyConfig = require('./lib/config')
+
+fs.readdir(oegyConfig.siteRoot, function(err, files) {
+  var dirs = _.each(files, function(file) {
+    return fs.statSync(oegyConfig.siteRoot + "/" + file).isDirectory()
+  })
+  console.log(dirs)
+})
 
 var app = express();
 
